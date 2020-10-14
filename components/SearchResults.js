d585c9611c97860be66d6cb0597c6e2cba67964c
Max Givno <max.ryazanov@gmail.com>
2020-10-14 15:56:40 +0300

    Chaged presentation for search results

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import PageSection from "./PageSection"
 
 const SearchResults = ({ results }) => {
   if (!results)
@@ -8,28 +8,9 @@ const SearchResults = ({ results }) => {
         <p className='text-center text-muted'>Try to search something else</p>
       </div>
     )
-  const list = results.map((meal, i) => (
-    <Link key={i} href={`/recipe/${meal.idMeal}`}>
-      <a>
-        <li className='list-group-item media'>
-          <img
-            src={`${meal.strMealThumb}/preview`}
-            className='mr-3'
-            alt={meal.strMeal}
-            width='64'
-            height='64'
-          ></img>
-          {meal.strMeal}
-        </li>
-      </a>
-    </Link>
-  ))
-  return (
-    <div className='container'>
-      <p>Found: {list.length}</p>
-      <ul className='list-group mx-auto'>{list}</ul>
-    </div>
-  )
+  const resultsCount = results.length
+
+  return <PageSection title={`Found: ${resultsCount}`} meals={results} />
 }
 
 export default SearchResults
